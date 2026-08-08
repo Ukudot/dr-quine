caf987a851b71f1ec2ad2c34c24e547eb34f8e6e
Giovanni Panico <gpanico@c2r1s8.42roma.it>
2026-08-08 16:08:59 +0200

    First C exercise completed

---
+++ C/Colleen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// This comment is outside main function
+
+
+void quine() {
+	char var32 = 32;
+	char var9 = 9;
+	char var10 = 10;
+	char var34 = 34;
+	char *self = "#include%1$c<stdio.h>%3$c%3$c//%1$cThis%1$ccomment%1$cis%1$coutside%1$cmain%1$cfunction%3$c%3$c%3$cvoid%1$cquine()%1$c{%3$c%2$cchar%1$cvar32%1$c=%1$c32;%3$c%2$cchar%1$cvar9%1$c=%1$c9;%3$c%2$cchar%1$cvar10%1$c=%1$c10;%3$c%2$cchar%1$cvar34%1$c=%1$c34;%3$c%2$cchar%1$c*self%1$c=%1$c%4$c%5$s%4$c;%3$c%3$c%2$cprintf(self,%1$cvar32,%1$cvar9,%1$cvar10,%1$cvar34,%1$cself);%3$c}%3$c%3$cint%1$cmain()%1$c{%3$c%2$cquine();%3$c%2$c//%1$cThis%1$ccomment%1$cis%1$cinside%1$cmain%1$cfunction%3$c%2$creturn%1$c0;%3$c}%3$c";
+
+	printf(self, var32, var9, var10, var34, self);
+}
+
+int main() {
+	quine();
+	// This comment is inside main function
+	return 0;
+}
