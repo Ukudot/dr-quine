c14d4f6a4dd0ca6bd1d5fbe15a26e0f91e210b58
Giovanni Panico <gpanico@c2r1s6.42roma.it>
2026-08-16 16:37:16 +0200

    Added new quinizer class to convert C program in macro-style C program with a quine function injected

--- utils/quinezer.py
+++ utils/quinezer.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 class Quinizer:
     def __init__(self, source_file):
@@ -10,19 +11,19 @@ class Quinizer:
         with open(self._source_file, "rb") as file:
             self._source_program = file.read().decode(decoder)
 
-    def _load_qnz_program(self, encoder: str = "utf-8") -> None:
+    def _load_qnz_program(self, encoder: str = "utf-8", mode: str = "") -> None:
         if self._quinized is None:
             print("The program isn't quinized yet")
             return
-        with open(self._source_file.split('.c')[0] + "_quine.c", "wb") as file:
+        with open(self._source_file.split('.c')[0] + f"{'_' + mode if mode != '' else ''}_quine.c", "wb") as file:
             file.write(self._quinized.encode(encoder))
 
-    def _escape_char_read(self) -> list:
+    def _escape_char_read(self, initial_set: set = {}) -> list:
         if self._source_program is None:
             print("The program isn't loaded yet")
             return
         # Initialize escape_chars with new line, double quotes and tab
-        escape_chars = {10, 9, 34}
+        escape_chars = initial_set
 
         # Cycle through each char and insert into escape chars if is an escapable character
         for char in self._source_program:
@@ -47,6 +48,7 @@ class Quinizer:
 class StandardQuinizer(Quinizer):
     def __init__(self, source_file):
         super().__init__(source_file)
+        self._mode = "standard"
 
     def _injector(self, esc_chars: list, replica: str = None) -> str:
         if self._source_program is None:
@@ -73,7 +75,7 @@ class StandardQuinizer(Quinizer):
         self._dump_src_program()
 
         # Reads the source program and creates the list of escapable characters found in the program
-        esc_chars = self._escape_char_read()
+        esc_chars = self._escape_char_read({9, 10, 34})
 
         # Creates a source program version with the quine function injected.
         # This version will be used as 'Documentation' to recreate the program itself
@@ -84,7 +86,80 @@ class StandardQuinizer(Quinizer):
 
         # Injects the program 'Documentation' in the quine function
         self._quinized = self._injector(esc_chars, replica)
-        self._load_qnz_program()
+        self._load_qnz_program(mode=self._mode)
+
+
+class MacroQuinizer(Quinizer):
+    def __init__(self, source_file):
+        super().__init__(source_file)
+        self._includes = []
+        self._mode = "macro"
+
+    def _extract_includes(self) -> None:
+        regex = "#include.*"
+
+        p = re.compile(regex)
+        self._includes = p.findall(self._source_program)
+        self._source_program = re.sub(regex, '',self._source_program)
+
+    def _injector(self, esc_chars: list, replica: str = None) -> str:
+        # Checks if the source program has been dumped
+        if self._source_program is None:
+            print("The program isn't loaded yet")
+            return
+
+        # Checks the includes and append the one needed by quine
+        if "#include <stdio.h>" not in self._includes:
+            self._includes.append("#include <stdio.h>")
+
+        # Creates the list of variables used to escape characters
+        quine_variables_str = [f"char var{char} = {char};" for char in esc_chars]
+        quine_variables = [f"var{char}" for char in esc_chars]
+
+        # Defines the SELF macro, it contains the 'Documentation' of the program
+        if replica is None:
+            self_macro = f"#define SELF \"%{len(esc_chars) + 1}$s\""
+        else:
+            self_macro =  f"#define SELF \"{replica}\""
+
+        # Defines the quine macro
+        quine_function = "\n#define quine(str) " + ''.join(quine_variables_str) + f"FILE *stream = fopen(\"{self._source_file.split('.c')[0]}_kid.c\",\"w\"); fprintf(stream, str, " + ', '.join(quine_variables) + ", str); fclose(stream)"
+
+        # Injects the quine macro in the main function
+        quine_function_call = "\nint main() {\nquine(SELF);\n"
+        splitted_source_program = self._source_program.split('int main() {\n')
+        injected_source_program = splitted_source_program[0] + quine_function_call + splitted_source_program[1]
+
+        # Linearizes the program
+        one_line_source_program = injected_source_program.replace('\n', '')
+
+        # Creates the macro version of the program
+        macro_source_program = '\n'.join(self._includes) + f"\n#define MAIN {one_line_source_program}\n{quine_function}\n{self_macro}\n\n//This MAIN macro runs the program\nMAIN\n"
+        return macro_source_program
+
+    def quinize(self) -> None:
+        # Dumps the source program into memory
+        self._dump_src_program()
+
+        # Remove from source_program the tabs
+        self._source_program = self._source_program.replace('\t', '')
+
+        # Extract from source program the includes instructions
+        self._extract_includes()
+
+        # Reads the source program and creates the list of escapable characters found in the program
+        esc_chars = self._escape_char_read({10, 34})
+
+        # Creates a source program version with the quine function injected.
+        # This version will be used as 'Documentation' to recreate the program itself
+        program_recipe = self._injector(esc_chars)
+
+        # Replaces each escapable characters with its ASCII code
+        replica = self._escape_char_replace(program_recipe, esc_chars)
+
+        # Injects the program 'Documentation' in the quine function
+        self._quinized = self._injector(esc_chars, replica)
+        self._load_qnz_program(mode=self._mode)
 
 
 class QuinizerFactory:
@@ -94,7 +169,7 @@ class QuinizerFactory:
     def quinizer(self, mode):
         match mode.lower():
             case "macro":
-                return None;
+                return MacroQuinizer(self._source_file);
             case "standard":
                 return StandardQuinizer(self._source_file);
             case _:
@@ -107,13 +182,4 @@ if __name__ == '__main__':
     quinized = ""
 
     QuinizerFactory(filepath).quinizer("standard").quinize()
-
-#    with open(filepath, 'rb') as file:
-#        source_program = file.read().decode("utf-8")
-#        esc_chars = escape_char_reader(source_program)
-#        injected_source_program = injector(source_program, esc_chars)
-#        replica = quinizer(injected_source_program, esc_chars)
-#        quinized = injector(source_program, esc_chars, replica)
-#
-#    with open(filepath.split('.c')[0] + ".quine.c", "wb") as file:
-#        file.write(quinized.encode("utf-8"))
+    QuinizerFactory(filepath).quinizer("macro").quinize()
